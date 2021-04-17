09cd52bdaa4059b0135078b6e6f038e370120119
RaulZaragoza1990 <zaragozalopez.raul@gmail.com>
2021-04-17 14:03:02 -0500

    nvabar implementention 0.0.02

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Navbar from './navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div>
+        Hola Mundo!
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/navbar.css';
+
+const navbar = () => {
+    return (
+        <div>
+            <a class="navbar-brand" href="#">
+                <img src="..\public\images\OndasNavbar.svg" ></img>
+            </a>
+        </div>
+    );
+  };
+
+  export default navbar;
